9c0c6af5e4210a1c752e846badaa687b6c8279ca
Flynn Bundy <flynn.sbundy@gmail.com>
2016-04-15 13:53:01 +0200

    Update rootcreation.ps1

--- dsc/rootcreation.ps1
+++ dsc/rootcreation.ps1
@@ -1,11 +1,13 @@
-#Install-module xDSCResourceDesigner
-
-New-xDscResource –Name ContainerDeploy -Property @(
+New-xDscResource –Name ContainerDeployment -Property @(
     New-xDscResourceProperty –Name ContainerName –Type String –Attribute Key
-    New-xDscResourceProperty –Name PortMapping –Type String –Attribute Write, Read
-    New-xDscResourceProperty –Name GitRootPath –Type String  –Attribute Write, Read
+    New-xDscResourceProperty –Name PortMapping –Type String –Attribute Key 
+    New-xDscResourceProperty –Name ProjectRoot –Type String  –Attribute Key
+    New-xDscResourceProperty –Name GitProject –Type String  –Attribute Key
     New-xDscResourceProperty –Name ContainerImage –Type String –Attribute Key 
     New-xDscResourceProperty –Name Ensure –Type String –Attribute Key -ValidateSet 'Absent','Present'
 ) -Path "C:\Program Files\WindowsPowerShell\Modules\ContainerDeploy" -Verbose
 
-New-ModuleManifest -Path "C:\Program Files\WindowsPowerShell\Modules\ContainerDeploy\ContainerDeploy.psd1" -CompanyName bundyfx -Guid (New-Guid) -Author 'Flynn Bundy' -ModuleVersion '1.0.0'
+New-ModuleManifest -Path "C:\Program Files\WindowsPowerShell\Modules\ContainerDeploy\ContainerDeploy.psd1" -CompanyName Coolblue -Guid (New-Guid) -Author 'Flynn Bundy' -ModuleVersion '1.0.0'
+
+
+$LocalVersion = (Get-Content $ProjectRoot\files\dockerfile | Select-String 'Version.*' -AllMatches).Matches.Value
