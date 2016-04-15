3c01609e2eab607f8f2feb34eb0575fa567b587d
Flynn Bundy <flynn.sbundy@gmail.com>
2016-04-15 16:33:23 +0200

    Update rootcreation.ps1

--- dsc/rootcreation.ps1
+++ dsc/rootcreation.ps1
@@ -1,13 +1,11 @@
 New-xDscResource –Name ContainerDeployment -Property @(
     New-xDscResourceProperty –Name ContainerName –Type String –Attribute Key
-    New-xDscResourceProperty –Name PortMapping –Type String –Attribute Key 
-    New-xDscResourceProperty –Name ProjectRoot –Type String  –Attribute Key
-    New-xDscResourceProperty –Name GitProject –Type String  –Attribute Key
-    New-xDscResourceProperty –Name ContainerImage –Type String –Attribute Key 
+    New-xDscResourceProperty –Name PortMapping –Type String –Attribute Key
+    New-xDscResourceProperty –Name ProjectRootPath –Type String  –Attribute Key
+    New-xDscResourceProperty –Name GitProjectURL –Type String  –Attribute Key
+    New-xDscResourceProperty –Name ProjectType –Type String  –Attribute Key -ValidateSet 'Golang','Python','IIS'
+    New-xDscResourceProperty –Name ContainerImage –Type String –Attribute Key
     New-xDscResourceProperty –Name Ensure –Type String –Attribute Key -ValidateSet 'Absent','Present'
 ) -Path "C:\Program Files\WindowsPowerShell\Modules\ContainerDeploy" -Verbose
 
 New-ModuleManifest -Path "C:\Program Files\WindowsPowerShell\Modules\ContainerDeploy\ContainerDeploy.psd1" -CompanyName Coolblue -Guid (New-Guid) -Author 'Flynn Bundy' -ModuleVersion '1.0.0'
-
-
-$LocalVersion = (Get-Content $ProjectRoot\files\dockerfile | Select-String 'Version.*' -AllMatches).Matches.Value
