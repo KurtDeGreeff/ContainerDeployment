5ddb55ea3bcd93a875c09abdc34de6fd776f027c
Flynn Bundy <flynn.sbundy@gmail.com>
2016-04-16 21:21:16 +0200

    Create webserver.go

---
+++ files/golang/webserver.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+ “fmt”
+ “os”
+ “github.com/gorilla/mux”
+ “net/http”
+ )
+
+func HelloWorld(w http.ResponseWriter, r *http.Request){
+
+                 w.Write([]byte(“Hello World!!!”))
+ }
+
+func main(){
+
+            r := mux.NewRouter()
+            r.HandleFunc(“/”, HelloWorld)
+            http.ListenAndServe(“:8080”,r)
+ }
