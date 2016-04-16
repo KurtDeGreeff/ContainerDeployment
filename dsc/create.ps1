6e0e4f5eb4b105d1166ec128630bfedc0f74a9fe
Flynn Bundy <flynn.sbundy@gmail.com>
2016-04-16 17:26:57 +0200

    Update create.ps1

--- dsc/create.ps1
+++ dsc/create.ps1
@@ -6,28 +6,28 @@ Import-DscResource -ModuleName ContainerDeploy
     node ("localhost")
     {
         ContainerDeployment 'WebContainer' {
-            ContainerName = 'webdev'
+            ContainerName = 'Web-dev'
             PortMapping = '86:80'
             ProjectRootPath = 'C:\git\iis'
-            ContainerImage = 'mywebapp'
+            ContainerImage = 'MyWebAPP'
             ProjectType = 'IIS'
             GitProjectURL = 'https://github.com/bundyfx/dockerimages.git'
             Ensure = 'Present'
         }
         ContainerDeployment 'DevDjangoContainer' {
-            ContainerName = 'djangodev'
-            PortMapping = '87:80'
+            ContainerName = 'Django-dev'
+            PortMapping = '8000:8000'
             ProjectRootPath = 'C:\git\Django'
-            ContainerImage = 'mydjangoapp'
+            ContainerImage = 'MyDjangoAPP'
             ProjectType = 'Python'
             GitProjectURL = 'https://github.com/bundyfx/dockerimages.git'
             Ensure = 'Present'
         }
         ContainerDeployment 'DevGoContainer' {
-            ContainerName = 'godev'
+            ContainerName = 'go-dev'
             PortMapping = '88:80'
             ProjectRootPath = 'C:\git\golang'
-            ContainerImage = 'mygolangapp'
+            ContainerImage = 'MyGoAPP'
             GitProjectURL = 'https://github.com/bundyfx/dockerimages.git'
             ProjectType = 'Golang'
             Ensure = 'Present'
