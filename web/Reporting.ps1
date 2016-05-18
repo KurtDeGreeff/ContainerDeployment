c8dd5c259e147268f3f108cf0c3af4501cf1e42d
Flynn Bundy <flynn.sbundy@gmail.com>
2016-05-18 19:23:09 +0200

    WIP
    WIP

---
+++ web/Reporting.ps1
@@ -0,0 +1,89 @@
+$Config = docker inspect (docker ps -a -q) | ConvertFrom-Json
+
+ $TableAdd = [pscustomobject]@{
+            Name            = $Config
+            Image           = $Config
+            Port            = $Config
+            Status          = $Config
+            PID             = $Config
+            Command         = $Config
+            }
+
+        [Array]$Output +=      
+         @"
+        </tr>
+        <tr>
+        <td>$($TableAdd.Name)</td>
+        <td>$($TableAdd.Image)</td>
+        <td>$($TableAdd.Port)</td>
+        <td>$($TableAdd.Status)</td>
+        <td>$($TableAdd.PID)</td>
+        <td>$($TableAdd.Command)</td>
+        </tr>   
+"@  
+
+
+$Table = @"
+    				<table border="1">
+                    <tr>
+                    <th>Name</th>
+                    <th>Image</th>
+                    <th>Port</th>
+                    <th>Status</th>
+                    <th>PID</th>
+                    <th>Command</th>
+                    $Output
+                    </table>
+"@
+
+@"
+<!DOCTYPE HTML>
+<!--
+	Spectral by HTML5 UP
+	html5up.net | @n33co
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+-->
+<html>
+	<head>
+		<title>Shipyard</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		<!--[if lte IE 8]><script src="assets/js/ie/html5shiv.js"></script><![endif]-->
+		<link rel="stylesheet" href="assets/css/main.css" />
+		<!--[if lte IE 8]><link rel="stylesheet" href="assets/css/ie8.css" /><![endif]-->
+		<!--[if lte IE 9]><link rel="stylesheet" href="assets/css/ie9.css" /><![endif]-->
+	</head>
+	<body class="landing">
+
+		<!-- Page Wrapper -->
+			<div id="page-wrapper">
+
+				<!-- Header -->
+					<header id="header" class="alt">
+						<h1><a href="index.html">Shipyard</a></h1>
+					</header>
+
+				<!-- Banner -->
+					<section id="banner">
+						<div class="inner">
+							<h2>Shipyard</h2>
+							<p>Deploy, Manage and Monitor Windows Server Containers</p>
+                            $Table
+						</div>
+					</section>
+
+
+			</div>
+
+		<!-- Scripts -->
+			<script src="assets/js/jquery.min.js"></script>
+			<script src="assets/js/jquery.scrollex.min.js"></script>
+			<script src="assets/js/jquery.scrolly.min.js"></script>
+			<script src="assets/js/skel.min.js"></script>
+			<script src="assets/js/util.js"></script>
+			<!--[if lte IE 8]><script src="assets/js/ie/respond.min.js"></script><![endif]-->
+			<script src="assets/js/main.js"></script>
+
+	</body>
+</html>
+"@ | Out-File C:\inetpub\wwwroot\index.html -Encoding utf8 -Force
