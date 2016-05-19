9aa65443a65cecf68254fee72c880fd811b223b9
Flynn Bundy <f.bundy@coolblue.nl>
2016-05-19 09:11:08 +0200

    Update Reporting.ps1

--- web/Reporting.ps1
+++ web/Reporting.ps1
@@ -63,11 +63,6 @@ $Table = @"
 
 @"
     <!DOCTYPE HTML>
-    <!--
-	    Spectral by HTML5 UP
-	    html5up.net | @n33co
-	    Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
-    -->
     <html>
 	    <head>
 		    <title>Shipyard</title>
@@ -114,4 +109,53 @@ $Table = @"
     </html>
 "@ | Out-File C:\inetpub\wwwroot\index.html -Encoding utf8 -Force
 
+} else {
+
+    <!DOCTYPE HTML>
+    <html>
+	    <head>
+		    <title>Shipyard</title>
+		    <meta charset="utf-8" />
+		    <meta name="viewport" content="width=device-width, initial-scale=1" />
+		    <!--[if lte IE 8]><script src="assets/js/ie/html5shiv.js"></script><![endif]-->
+		    <link rel="stylesheet" href="assets/css/main.css" />
+		    <!--[if lte IE 8]><link rel="stylesheet" href="assets/css/ie8.css" /><![endif]-->
+		    <!--[if lte IE 9]><link rel="stylesheet" href="assets/css/ie9.css" /><![endif]-->
+                    <meta http-equiv="refresh" content="30" >
+	    </head>
+	    <body class="landing">
+
+		    <!-- Page Wrapper -->
+			    <div id="page-wrapper">
+
+				    <!-- Header -->
+					    <header id="header" class="alt">
+						    <h1><a href="index.html">Shipyard</a></h1>
+					    </header>
+
+				    <!-- Banner -->
+					    <section id="banner">
+						    <div class="inner">
+							    <h2>Shipyard</h2>
+							    <p>Deploy, Manage and Monitor Windows Server Containers</p>
+                                                            <p>Currently there are no Containers running</p>
+						    </div>
+					    </section>
+
+
+			    </div>
+
+		    <!-- Scripts -->
+			    <script src="assets/js/jquery.min.js"></script>
+			    <script src="assets/js/jquery.scrollex.min.js"></script>
+			    <script src="assets/js/jquery.scrolly.min.js"></script>
+			    <script src="assets/js/skel.min.js"></script>
+			    <script src="assets/js/util.js"></script>
+			    <!--[if lte IE 8]><script src="assets/js/ie/respond.min.js"></script><![endif]-->
+			    <script src="assets/js/main.js"></script>
+
+	    </body>
+    </html>
+"@ | Out-File C:\inetpub\wwwroot\index.html -Encoding utf8 -Force
+	
 }
